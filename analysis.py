29d8223a6945defa6574e0b1d8c2824b59df5123
atyn104 <s22a0014@siswa.umk.edu.my>
2026-01-04 21:05:40 +0800

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import seaborn as sns
+import plotly.graph_objects as go
+import numpy as np
 
 # 1. Load your data
 # Ensure your CSV file is in the same folder as this script
